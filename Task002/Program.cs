b1d236c5d4ee84789ababd9358ad88fdbfb8200b
Anna <annkrv80@yandex.ru>
2022-08-07 00:22:31 +0700

    Алгоритм решения задачи

---
+++ Task002/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите первое число: "); 
+string numberFirst=Console.ReadLine()??"";
+
+int numberA;
+int numberB;
+
+if(int.TryParse(numberFirst, out numberA))  //Проверяем первый введенный символ, если это число, то программа продолжит работу. 
+
+{Console.WriteLine("Введите второе число: ");
+string numberSecond=Console.ReadLine()??"";
+
+    if(int.TryParse(numberSecond, out numberB))
+    {
+     
+        if (numberA>numberB)
+            {Console.Write("Max");
+            Console.WriteLine(numberA);
+            }
+      
+        else 
+            {
+            Console.Write("Max");
+            Console.WriteLine(numberB);
+            }
+
+    }
+   else
+   {
+    Console.WriteLine ("Ошибка! Введите число!");
+   }
+}   
+else
+{
+ Console.WriteLine ("Ошибка! Введите число!");
+}
+
