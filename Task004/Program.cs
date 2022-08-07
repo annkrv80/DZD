377a491d5af06f9df09d40f21b0b24524865f240
Anna <annkrv80@yandex.ru>
2022-08-07 15:29:06 +0700

    алгоритм решения задачи

---
+++ Task004/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Введите перое число...");
+string numberFirst=Console.ReadLine()??"";
+int a; 
+int b;
+int c;
+int max=0;
+
+if(int.TryParse(numberFirst,out a))
+{ max=a;
+  Console.WriteLine("Введите второе число...");
+  string numberSecond=Console.ReadLine()??"";
+    if(int.TryParse(numberSecond, out b))
+    {Console.WriteLine("Введите третье число");
+     string numberThird=Console.ReadLine()??"";
+     if (int.TryParse(numberThird, out c))
+      {if (a<b)
+      {max=b;}
+
+      if (b<c)
+      {max=c;}
+        
+      Console.Write("max");
+      Console.WriteLine (max);
+
+    
+
+
+      }
+      else
+      {
+        Console.WriteLine ("Ошибка! Введите число!");
+      }
+
+
+
+
+    }
+    else
+    {
+      Console.WriteLine("Ошибка!Введите число!");
+    }
+
+
+
+
+}
+else
+{
+ Console.WriteLine ("Ошибка! Введите число!");
+}
