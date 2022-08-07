641899f91de7efd237b6526453aaee31a0f624c2
Anna <annkrv80@yandex.ru>
2022-08-07 19:50:05 +0700

    решение задачи 8

---
+++ Task008/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int N=int.Parse(Console.ReadLine()??"");
+int x;
+int count = N;
+while (N>1)
+{x=N-1;
+   if((x%2)==0)
+   {
+    Console.WriteLine(x);
+   }
+   count=count-1;
+
+}
+
+
+
