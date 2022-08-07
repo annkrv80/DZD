997d2d7a54e4ca18b35b3fcbf8e7be0653411769
Anna <annkrv80@yandex.ru>
2022-08-07 21:24:05 +0700

    задача через for

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.WriteLine("Введите число:");
 int n =int.Parse(Console.ReadLine()??"");
-for (int i =1; i <=n; i++)
-     if (i % 2 ==0)
-        Console.Write(i);
+for (int i =2; i <=n; i+=2)
+     Console.Write(i);
 
 
 
