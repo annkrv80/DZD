72ea035421aeb497da1fec30b49ac29754b5acff
Anna <annkrv80@yandex.ru>
2022-08-11 22:55:18 +0700

    ввод день недели на выход проверка выходной или рабочий день

---
+++ Task015/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int num = new Random(). Next(1,8);
+
+if(num==6 || num==7)
+{
+    Console.WriteLine($"{num} выходной день");
+}
+    
+else
+{
+    Console.WriteLine($"{num} рабочий день");
+}
