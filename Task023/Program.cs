5c8991fafb1a05fdd63b0cbfdca5c1c40c545bb3
Anna <annkrv80@yandex.ru>
2022-08-14 22:47:32 +0700

    Вывод таблицы кубов от 1 до N

---
+++ Task023/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+int n = int.Parse(Console.ReadLine() ?? "");
+
+int i = 1;
+double result = 1;
+
+while (i <= n)
+{
+    result = Math.Pow(i, 3);
+    Console.Write($"{result} ");
+    i++;
+}
