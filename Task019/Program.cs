32157d42d89981d91d12a43db70cfc64c13e194c
Anna <annkrv80@yandex.ru>
2022-08-14 21:54:16 +0700

    Ввели метод поиска числа и ограничение на ввод пятизначного числа

--- Task019/Program.cs
+++ Task019/Program.cs
@@ -1,23 +1,36 @@
-﻿Console.Clear();
-int i=0;
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
 
 Console.Write("Введите пятизначное число ");
 string str = Console.ReadLine()??"";
+
 int[] number = new int[str.Length];
-for( i=0; i< str.Length; i++)
+
+void SearchPalindrome(string str)
 {
-   number[i] = int.Parse(str[i].ToString());
-}
-if(number[0]==number[4] && number[1]==number[3])
+    for(int i=0; i< str.Length; i++)
+    {
+        number[i] = int.Parse(str[i].ToString());
+    }
+        if(number[0]==number[4] && number[1]==number[3])
+        {
+            Console.Write($"{str} палиндром"); 
+        }
+        else
+        {
+             Console.Write($"{str} не палиндром");  
+        }
+}    
+
+if (str.Length==5)
 {
-    Console.Write($"{str} палиндром"); 
+    SearchPalindrome(str);
 }
 else
 {
-   Console.Write($"{str} не палиндром");  
+    Console.WriteLine("Ошибка введите пятизначное число");
 }
-        
-
 
 
 
