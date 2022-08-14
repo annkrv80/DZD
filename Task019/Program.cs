0ddd151106565c7abde0991093dee8e83b53927e
Anna <annkrv80@yandex.ru>
2022-08-14 19:04:12 +0700

    Первое рабочее решение

---
+++ Task019/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+int i=0;
+
+Console.Write("Введите пятизначное число ");
+string str = Console.ReadLine()??"";
+int[] number = new int[str.Length];
+for( i=0; i< str.Length; i++)
+{
+   number[i] = int.Parse(str[i].ToString());
+}
+if(number[0]==number[4] && number[1]==number[3])
+{
+    Console.Write($"{str} палиндром"); 
+}
+else
+{
+   Console.Write($"{str} не палиндром");  
+}
+        
+
+
+
+
+
+
+
+
+
+ 
