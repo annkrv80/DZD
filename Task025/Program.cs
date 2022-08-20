2b6bc77c297684cc80f51860c3bf17c6173bc012
Anna <annkrv80@yandex.ru>
2022-08-20 23:03:20 +0700

    Возведение числа А в степень числа В

---
+++ Task025/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+int num1 = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+int num2 = GetNumberFromUser("Введите целое число В: ", "Ошибква ввода!");
+int result = Exponentiation(num1);
+
+Console.WriteLine($"\t {num1} в степени {num2} равно {result}");
+
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int Exponentiation(int rank)
+{
+    rank = 1;
+    int count = 0;
+    while (count < num2)
+    {
+        rank = rank * num1;
+        count++;
+    }
+    return rank;
+}
+
+
+
