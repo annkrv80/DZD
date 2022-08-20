2d2a34b87d3babf1b1f164b5ce93e63dddcc7f54
Anna <annkrv80@yandex.ru>
2022-08-21 01:19:45 +0700

    ВВедение элементов массива

---
+++ Task029/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+
+int [] array = new int [8];
+
+for (int i=0; i<8; i++)
+{
+    Console.Write($"Введите элемент массива под индексом {i}:\t");
+    array[i]=int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Вывод масcива");
+for (int i=0; i<8; i++)
+{
+    Console.WriteLine(array[i]);
+    
+}
