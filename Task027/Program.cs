19e95f556326f18fad5c3b0abbf9126e204f0b0d
Anna <annkrv80@yandex.ru>
2022-08-20 23:34:06 +0700

    Нахождение суммы цифр в числе

---
+++ Task027/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+
+int num = GetNumberFromUser("Введите целое число:", "Ошибка вводаI");
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+   }
+
+}
+int result = SumDigits(num);
+
+int SumDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number = number / 10;
+    }
+
+    return sum;
+
+}
+
+Console.WriteLine($"сумма цифр число {num} равна {result}");
