4777c602cc6c9d6c1afe69175edd6f2dbb49b04f
Anna <annkrv80@yandex.ru>
2022-08-23 22:11:35 +0700

    нахождение суммы элементов стоящих на нечетных позициях в массиве

---
+++ Task036/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+Console.Clear();
+
+int[] array = GetArray(10, -15, 15);
+Console.WriteLine(String.Join(" ", array));
+int oddNumberSum = GetOddNumberSum(array);
+Console.WriteLine($"Сумма элеметов, стоящих на нечетных позициях равна {oddNumberSum}");
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+int GetOddNumberSum(int[] array)
+{
+    int oddNumberSum = 0;
+
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        oddNumberSum += array[i];
+
+    }
+
+    return oddNumberSum;
+}
+
